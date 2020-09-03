dcdd1cfc9567c4d2d6f9fd133762d4acb0cff2b1
Jonathan Cardoso Machado <me@jonathancardoso.com>
2020-09-03 13:23:36 -0300

    do not import the whole react-feather library

--- src/components/icon/FeatherIcons.tsx
+++ src/components/icon/FeatherIcons.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import * as FeatherOriginalIcons from 'react-feather'
+import {
+  Calendar,
+  Folder,
+  GitHub,
+  Instagram,
+  Linkedin,
+  Tag,
+  Twitter,
+  Props as FeatherIconProps,
+} from 'react-feather'
 import { Box, BoxProps } from 'rebass'
 
+// We are not using * as icons on the import above to not import the whole package.
+const icons = { Calendar, Tag, Folder, Twitter, GitHub, Linkedin, Instagram } as const
+
 type FeatherIconsMap = {
-  [iconName in keyof typeof FeatherOriginalIcons]: React.FC<
-    FeatherOriginalIcons.Props & BoxProps
-  >
+  [iconName in keyof typeof icons]: React.FC<FeatherIconProps & BoxProps>
 }
 
 export const FeatherIcons = {} as FeatherIconsMap
 
-for (const [iconName, Icon] of Object.entries(FeatherOriginalIcons)) {
+for (const [iconName, Icon] of Object.entries(icons)) {
   // @ts-ignore
   FeatherIcons[iconName] = (props) => (
     <Box
